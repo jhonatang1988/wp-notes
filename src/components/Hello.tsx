220d1923fec4d513d215146f56ba5fd31a3eacb4
Jhonatan Galindo <jhonatan1988@gmail.com>
2023-11-05 18:27:32 -0500

    add navigation to signup and login

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,3 +1,13 @@
 export default function Hello({ hello }: { hello: string }) {
-  return <div>hello</div>;
+  return (
+    <div>
+      <p>{hello}</p>
+      <div>
+        <a href='/login'>Login</a>
+      </div>
+      <div>
+        <a href='/signup'>Signup</a>
+      </div>
+    </div>
+  );
 }
